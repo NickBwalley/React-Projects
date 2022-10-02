6f28f7472c589228a8eac5194b33b0b867c74d0f
NickBwalley <nickbwalley@gmail.com>
2022-10-02 22:26:08 +0300

    accordion reactPractice

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import questions from './data';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from 'react'
+import questions from './data'
+import data from './data'
+import SingleQuestion from './Question'
 function App() {
-  return <main>
-    <div className='container'>
-      <h3>questions and answers about login</h3>
-      <section className='info'>
-        {
-          questions.map((question) => {
+  return (
+    <main>
+      <div className='container'>
+        <h3>questions and answers about login</h3>
+        <section className='info'>
+          {questions.map((question) => {
             return <SingleQuestion key={question.id} {...question} />
-          })
-        }
-      </section>
-    </div>
-  </main>;
+          })}
+        </section>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
