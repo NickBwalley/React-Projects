674c6611515c4aab627a6b1d0925826aae244500
NickBwalley <nickbwalley@gmail.com>
2022-10-04 20:06:31 +0300

    tabs menu project practice react

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -5,7 +5,7 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
   const [loading, setLoading] = useState(true)
-  const [job, setJobs] = useState([])
+  const [jobs, setJobs] = useState([])
   const [value, setValue] = useState(0)
 
   const fetchJobs = async () => {
@@ -27,7 +27,32 @@ function App() {
     )
   }
 
-  return <h2>Jobs</h2>
+  const { company, dates, duties, title } = jobs[value]
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* btn container */}
+        {/* job info */}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default App
