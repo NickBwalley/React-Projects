6f35c35f00d45f03cc07e5a1c73dd594b8354a8c
NickBwalley <nickbwalley@gmail.com>
2022-10-14 21:06:35 +0300

    lorem ipsum generator

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -6,6 +6,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    let amount = parseInt(count)
+    if (count <= 0) {
+      amount = 1
+    }
+    if (count > 8) {
+      amount = 8
+    }
+    setText(data.slice(0, amount))
   }
   return (
     <section className='section-center'>
@@ -24,14 +32,9 @@ function App() {
         </button>
       </form>
       <article className='lorem-text'>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita,
-          doloribus.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita,
-          doloribus.
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
       </article>
     </section>
   )
