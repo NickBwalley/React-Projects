0bd34245274a60702a181253615b9087e7eea5ce
NickBwalley <nickbwalley@gmail.com>
2022-10-18 20:13:21 +0300

    color generator project

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -6,18 +6,19 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
-  const [list, setList] = useState([])
+  const [list, setList] = useState(new Values('#f15025').all(10))
 
   const handleSubmit = (e) => {
     e.preventDefault()
     try {
       let colors = new Values(color).all(10)
-      console.log(colors)
+      setList(colors)
     } catch (error) {
       setError(true)
       console.log(error)
     }
   }
+
   return (
     <>
       <section className='container'>
@@ -26,9 +27,7 @@ function App() {
           <input
             type='text'
             value={color}
-            onchange={(e) => {
-              setColor(e.target.value)
-            }}
+            onChange={(e) => setColor(e.target.value)}
             placeholder='#f15025'
             className={`${error ? 'error' : null}`}
           />
@@ -38,7 +37,16 @@ function App() {
         </form>
       </section>
       <section className='colors'>
-        <h4>List goes here!</h4>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
       </section>
     </>
   )
