b0520c0757841634aee8168494e1075ab30c220e
NickBwalley <nickbwalley@gmail.com>
2022-10-22 15:07:44 +0300

    CRUD complete grocerybud miniproject

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -19,7 +19,18 @@ function App() {
       // display alert
       showAlert(true, 'danger', 'please enter value')
     } else if (name && isEditing) {
-      //deal with edit
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name }
+          }
+          return item
+        })
+      )
+      setName('')
+      setEditID(null)
+      setIsEditing(false)
+      showAlert(true, 'success', 'Value changed')
     } else {
       showAlert(true, 'success', 'item added to the list')
       const newItem = { id: new Date().getTime().toString(), title: name }
@@ -40,6 +51,13 @@ function App() {
   const removeItem = (id) => {
     showAlert(true, 'danger', 'Item removed')
     setList(list.filter((item) => item.id !== id))
+  }
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id)
+    setIsEditing(true)
+    setEditID(id)
+    setName(specificItem.title)
   }
 
   return (
@@ -64,7 +82,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className='grocery-container'>
-          <List items={list} removeItem={removeItem} />
+          <List items={list} removeItem={removeItem} editItem={editItem} />
           <button className='clear-btn' onClick={clearList}>
             clear items
           </button>
